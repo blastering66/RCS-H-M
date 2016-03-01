24006136af511d18239991a63bdf8b42c40b231c
ridho <ridho_animage@yahoo.com>
2016-03-01 19:40:47 +0700

    ubah nomer kirim

--- app/src/main/java/id/tech/hsmsjacket/Parameter_Collections.java
+++ app/src/main/java/id/tech/hsmsjacket/Parameter_Collections.java
@@ -4,7 +4,7 @@ package id.tech.hsmsjacket;
  * Created by macbook on 2/4/16.
  */
 public class Parameter_Collections {
-    public static final String nomer_kirim = "+6281283676317";
+    public static final String nomer_kirim = "+6281280070020";
 
     public static final String nomer_holcim = "Holcim";
     public static final String nomer_holcim_2 = "(Holcim)";
